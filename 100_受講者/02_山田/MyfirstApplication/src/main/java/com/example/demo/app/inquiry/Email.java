8be7ca6584701113337ed54e3023daae3ad3381d
SyouheiYamada <s-yamada@mik-s.jp>
2023-09-28 11:32:49 +0900

    9/28 山田

---
+++ 100_受講者/02_山田/MyfirstApplication/src/main/java/com/example/demo/app/inquiry/Email.java
@@ -0,0 +1,5 @@
+package com.example.demo.app.inquiry;
+
+public @interface Email {
+
+}
---
+++ 100_受講者/02_山田/MyfirstApplication/src/main/java/com/example/demo/app/survey/SurveyForm.java
@@ -0,0 +1,26 @@
+package com.example.demo.app.survey;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import com.example.demo.app.inquiry.NotNull;
+import com.example.demo.app.inquiry.Size;
+
+import lombok.Data;
+
+@Data
+public class SurveyForm{
+
+    @Min(0)
+    @Max(150)
+    private int age;
+    
+    @Min(1)
+    @Max(5)
+    private int satisfaction;
+
+	@NotNull
+	@Size(min = 1, max = 200, message="Please input 200 characters or less")
+    private String comment;
+
+}
