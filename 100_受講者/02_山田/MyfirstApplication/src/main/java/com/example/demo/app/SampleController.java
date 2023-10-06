8dbaea72462f0316cf96dd19566f1676f5a8be6a
SyouheiYamada <s-yamada@mik-s.jp>
2023-10-06 17:05:09 +0900

    10/06 山田

--- 100_受講者/02_山田/MyfirstApplication/src/main/java/com/example/demo/app/SampleController.java
+++ 100_受講者/02_山田/MyfirstApplication/src/main/java/com/example/demo/app/SampleController.java
@@ -18,29 +18,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample")
 
 public class SampleController {
-	
 
-	private final JdbcTemplate jdbcTemplate;
+//	private final JdbcTemplate jdbcTemplate;
 
-	@Autowired
-	public SampleController(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+//	public SampleController(JdbcTemplate jdbcTemplate) {
+//		this.jdbcTemplate = jdbcTemplate;
+//	}
 
-	//GETリクエストをURLとコントローラーのクラスまたはメソッドに紐づけるためのアノテーション
-    //下記のようなメソッドの際につける
-	@GetMapping("/test")	
-	//ページの出力に対応するメソッド。このStringはHTMLのファイル名を返す
+	// GETリクエストをURLとコントローラーのクラスまたはメソッドに紐づけるためのアノテーション
+	// 下記のようなメソッドの際につける
+	@GetMapping("/test")
+	// ページの出力に対応するメソッド。このStringはHTMLのファイル名を返す
 	public String test(Model model) {
-		//SQL文を入力
-		String sql = "SELECT id,  name, email"
-				  +"FROM inquiry WHERE id = 1";
-		//上の行で既にSQLを作っているので変数sqlを代入
-		Map<String, Object> map = jdbcTemplate.queryForMap(sql);
-		//引数がStringとオブジェトのものを選ぶ
+		// SQL文を入力
+//		String sql = "SELECT id,  name, email" + "FROM inquiry WHERE id = 1";
+//		// 上の行で既にSQLを作っているので変数sqlを代入
+//		Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+//		// 引数がStringとオブジェトのものを選ぶ
 		model.addAttribute("title", "Inquiry Form");
-		model.addAttribute("name", map.get("name"));
-		model.addAttribute("email", map.get("email"));
+//		model.addAttribute("name", map.get("name"));
+//		model.addAttribute("email", map.get("email"));
 		return "test";
 
 	}
