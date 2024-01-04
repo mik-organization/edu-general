2d29311a9062d03040f90933dfc5c4b9c68ed211
unknown <m-mogami@mik-s.jp>
2024-01-04 17:11:04 +0900

    2024/01/04

---
+++ 100_受講者/05_最上/test00/Sample01.java
@@ -0,0 +1,12 @@
+package test00;
+public class Sample01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("山田太郎");
+		
+		//コメントの入れ方　一行の場合　＜その１＞
+		/*コメントの入れ方　複数行の場合　＜その２＞*/
+	}
+
+}
---
+++ 100_受講者/05_最上/test00/Sample06.java
@@ -0,0 +1,15 @@
+package test00;
+
+public class Sample06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		
+		System.out.println(--x);
+		System.out.println(x--);
+		System.out.println(x);
+
+	}
+
+}
