ea357884050f35e2669f5e2e165198cd9b1bcd9c
unknown <m-mogami@mik-s.jp>
2024-01-22 11:23:40 +0900

    2024/01/22

---
+++ 100_受講者/05_最上/workspase_API/edu_app2/src/main/java/com/spring/edu_app2/EduApp2Application.java
@@ -0,0 +1,13 @@
+package com.spring.edu_app2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduApp2Application.class, args);
+	}
+
+}
