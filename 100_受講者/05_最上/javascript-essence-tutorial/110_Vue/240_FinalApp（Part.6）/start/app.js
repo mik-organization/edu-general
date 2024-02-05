12391c0bd86b852c98292f366c20faed7585b169
unknown <m-mogami@mik-s.jp>
2024-02-05 15:32:20 +0900

    2024/02/05

--- 100_受講者/05_最上/javascript-essence-tutorial/110_Vue/240_FinalApp（Part.6）/start/app.js
+++ 100_受講者/05_最上/javascript-essence-tutorial/110_Vue/240_FinalApp（Part.6）/start/app.js
@@ -1,18 +1,41 @@
 import { nodeOps } from './nodeOps.js';
 import { createVNode, patch } from './renderer.js';
 import { reactive, computed, effect } from './reactive.js';
+
 function createApp(args) {
     const { data, computed: computedData, methods, render } = args;
 
     const app = {};
 
+    const rawData  = data();
+    const ctx = { ...rawData, ...computedData, ...methods};
+
+    app.publicCtx = new Proxy(ctx, { 
+        get (target, key, receivar){
+            if(rawData.hasOwnProperty(key)){
+                return app.data[key];
+            } else if(computedData.hasOwnProperty(key)){
+                return app.computed[key].value;
+            }else{
+                return Reflect.get(target, key, receivar)
+            }
+        },
+        set (target, key, value, receivar){
+            if(rawData.hasOwnProperty(key)){
+                const res = app.data[key] = value;
+                return res;
+            }
+        }
+    })
+    console.log(ctx);
+
     app.methods = methods;
     
-    app.data = reactive(data());
+    app.data = reactive(rawData);
 
     app.computed = {};
     for(const prop in computedData) {
-        const c = computed(computedData[prop], app);
+        const c = computed(computedData[prop], app.publicCtx);
         app.computed[prop] = c;
     }
 
@@ -20,7 +43,7 @@ function createApp(args) {
         const container = nodeOps.qs(selector);
         app.vnode = createVNode();
         effect(() => {
-            const nextVNode = render.call(app);
+            const nextVNode = render.call(app.publicCtx);
             console.log('%c[patch]', 'background: orange; color: white;', app.vnode, nextVNode);
             patch(app.vnode, nextVNode, container);
             app.vnode = nextVNode;
