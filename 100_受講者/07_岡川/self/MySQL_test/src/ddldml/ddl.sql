1b7f8807f726f0e70d5e6791f53580aa3bca254c
okagawa-s <s-okagawa@mik-s.jp>
2024-06-03 17:00:52 +0900

    スキーマ追加再提出

--- 100_受講者/07_岡川/self/MySQL_test/src/ddldml/ddl.sql
+++ 100_受講者/07_岡川/self/MySQL_test/src/ddldml/ddl.sql
@@ -1,8 +1,9 @@
-CREATE schema sample;
 DROP schema sample;
+CREATE schema sample;
+
 use sample;
 CREATE TABLE vegetable(
-    id int comment 'id',
+    id int comment 'ID',
     name varchar(10) comment '名前',
     price int comment '値段'
 );
