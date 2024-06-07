3d425af2b5e2ae7c78f4cb27b6897a6f1b394ac6
okagawa-s <s-okagawa@mik-s.jp>
2024-06-07 11:23:33 +0900

    宣言場所の修正

--- 100_受講者/07_岡川/self/MySQL_test/src/MySQL_test/DBSampleDelete.java
+++ 100_受講者/07_岡川/self/MySQL_test/src/MySQL_test/DBSampleDelete.java
@@ -11,12 +11,12 @@ class DBSampleDelete {
 		final var URL = "jdbc:mysql://localhost:3306/sample";
 
 		try {
-			var con = DriverManager.getConnection(URL, ID, PASS);
-			System.out.println("接続成功");
-
 			final var SQL = "SELECT * FROM vegetable";
+			var con = DriverManager.getConnection(URL, ID, PASS);
 			var ps = con.prepareStatement(SQL);
 			var rs = ps.executeQuery();
+
+			System.out.println("---接続成功---");
 
 			while (rs.next()) {
 				var id = rs.getString("id");
@@ -26,13 +26,12 @@ class DBSampleDelete {
 				System.out.println(id + ":" + name + ":" + pay + "円");
 
 			}
-			final var SQL_DELETE = "delete from vegetable where name=?;";
-			ps = con.prepareStatement(SQL_DELETE);
-
 			var sc = new Scanner(System.in);
 			System.out.print("消去する野菜を入力してください:");
 			var deleteName = sc.nextLine();
 
+			final var SQL_DELETE = "delete from vegetable where name=?;";
+			ps = con.prepareStatement(SQL_DELETE);
 			ps.setString(1, deleteName);
 			ps.executeUpdate();
 
