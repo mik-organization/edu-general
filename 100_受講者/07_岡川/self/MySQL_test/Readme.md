4a194e179c4d32cd31863cdcff34ccf60d207976
yoshioka@mik-s.jp <yoshioka@mik-s.jp>
2025-06-02 12:21:46 +0900

    up

--- 100_受講者/07_岡川/self/MySQL_test/Readme.md
+++
@@ -1,8 +0,0 @@
-# 必要ソフトのインストール
-- MySQL
-- connecter/J
-# 開発環境構築（Eclipse前提）
-- テーブル作成（DDL使用）
-- gitからcloneしソースをインポート
-- connecter/J紐づけ
-# 操作方法
--- 100_受講者/07_岡川/self/MySQL_test/src/ddldml/ddl.sql
+++
@@ -1,9 +0,0 @@
-DROP schema sample;
-CREATE schema sample;
-
-use sample;
-CREATE TABLE vegetable(
-    id int comment 'ID',
-    name varchar(10) comment '名前',
-    price int comment '値段'
-);
